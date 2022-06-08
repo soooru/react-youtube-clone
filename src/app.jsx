86bb673ef6b4a51852bc9af45c375473a2f03bb3
soooru <threequaters9@gmail.com>
2022-06-09 01:02:59 +0900

    강의 8.8 검색 기능 프로처럼 리팩토링

--- src/app.jsx
+++ src/app.jsx
@@ -3,40 +3,17 @@ import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=AIzaSyBnc_pqVarUviFpMw-Ab0q7Z0pmJt1oQCY`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({
-          ...item,
-          id: item.id.videoId || item.id,
-        })),
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log('error', error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBnc_pqVarUviFpMw-Ab0q7Z0pmJt1oQCY',
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
